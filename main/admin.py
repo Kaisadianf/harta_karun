5573cb50f25d1ee45de60e499d0e3e21b5a4957e
Kaisa <kaisa.dian@ui.ac.id>
2023-09-06 21:03:07 +0700

    revisi admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from .models import Item  # Import the Item model from your models file
+from .models import Item  # Import the Item model from your models.py file
 
-# Create an admin class for the Item model
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'amount', 'description')  # Display these fields in the admin list view
-
-# Register the Item model with the admin site
-admin.site.register(Item, ItemAdmin)
-
+# Register your models here.
+admin.site.register(Item)  # Register the Item model with the admin panel
