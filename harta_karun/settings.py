f31bcfccc65b2cb6c2c12290b76027a94ce46522
Kaisa <kaisa.dian@ui.ac.id>
2023-09-06 20:44:34 +0700

    add test.py

--- harta_karun/settings.py
+++ harta_karun/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-95upm=g=h0n)&(o46$_tux_)ar0%xr&77srh15b-eul06@pc!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
