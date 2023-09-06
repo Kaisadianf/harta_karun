fc8ee4007c4ea672897085fe940c66acea3f9ba5
Kaisa <kaisa.dian@ui.ac.id>
2023-09-06 14:07:47 +0700

    menyambungkan direktori

---
+++ harta_karun/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for harta_karun project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harta_karun.settings')
+
+application = get_wsgi_application()
